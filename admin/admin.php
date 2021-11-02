87371b5710014e93dbfbcee63a1ec40789fa4e97
Christian Heinisch <chris@christian-heinisch.de>
2021-11-02 09:39:58 +0100

    Modify NAV Bar

--- admin/admin.php
+++ admin/admin.php
@@ -52,14 +52,18 @@
     
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Picture CMS</a>
+  <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-light">Content</a></li>
+        <li><a href="#" class="nav-link px-2 link-secondary">Settings</a></li>
+      </ul>
+      <div class="col-md-3 text-end">        
+        <div class="nav-item text-nowrap">
+          <a class="nav-link px-3" href="login.php?login=logoff">Sign out</a>
+        </div>
+      </div>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="login.php?login=logoff">Sign out</a>
-    </div>
-  </div>
 </header>
 
 <div class="container-fluid">
@@ -91,29 +95,7 @@
               Content
             </a>
           </li>          
-        </ul>
-
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>System</span>
-          <a class="link-secondary" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              System Settings
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              System Info
-            </a>
-          </li>
-        </ul>
-        
+        </ul>       
         
 <?php
   if($pagetype =='essay')

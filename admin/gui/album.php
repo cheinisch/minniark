e775a1999b0b325c095fd5e6ba3fd9cdec4a1fe8
Christian Heinisch <chris@christian-heinisch.de>
2021-11-02 10:40:51 +0100

    Add language files

--- admin/gui/album.php
+++ admin/gui/album.php
@@ -1,9 +1,28 @@
-
+<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Album List</span>
+          <a class="link-secondary" href="#" aria-label="Add a new report">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Album 1
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Album 2
+            </a>
+          </li>
+        </ul>
         </div>
-</nav>
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    </nav>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
+        <h1 class="h2">Album</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group me-2">
             <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>

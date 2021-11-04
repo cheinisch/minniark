aa5575d1e7b5cc29c3912695bba1d8ad58aff63c
Christian Heinisch <chris@christian-heinisch.de>
2021-11-04 21:05:40 +0100

    Modify Essay Detail Page
    Modify Essay Detail Page

--- admin/admin.php
+++ admin/admin.php
@@ -50,6 +50,7 @@
 
     <!-- Bootstrap core CSS -->
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
 
     <style>
       .bd-placeholder-img {

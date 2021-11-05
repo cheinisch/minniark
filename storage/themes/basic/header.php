4c2e9683c05b150972b8922e547757476408b76b
Christian Heinisch <chris@christian-heinisch.de>
2021-11-05 15:30:13 +0100

    Theme Modifications

--- storage/themes/basic/header.php
+++ storage/themes/basic/header.php
@@ -1,5 +1,30 @@
 <!doctype html>
 <html lang="en">
     <head>
-            <title><?php echo pcs_get_page_title(); ?></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Christian Heinisch">
+        <meta name="generator" content="Hugo 0.88.1">
+        <title><?php echo pcs_get_page_title(); ?></title>
+
+        <!-- CSS -->
+        <link href="<?php echo pcs_get_theme_path(); ?>css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- JS -->
     </head>
+    <body>
+        <div class="container">
+            <div class="text-center">
+                <h1 class="display-5"><?php echo pcs_get_page_title(); ?></h1>
+                <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Essays</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+            </div>
+        </div>

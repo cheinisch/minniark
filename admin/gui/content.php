ccd3a8e8ee2d4979249e2c9230e8696ab11e9216
Christian Heinisch <chris@christian-heinisch.de>
2021-11-17 15:12:26 +0100

    configure file uploader

--- admin/gui/content.php
+++ admin/gui/content.php
@@ -1,2 +1,18 @@
 </div>
     </nav>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Pictures</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+      <div class="btn-group me-2">
+        <!--<form action="#" method="post">-->
+        <a href="inc/file_uploader.php" class="btn btn-sm btn-outline-danger" onclick="pop_up(this);">Upload</a>
+      </div>
+    </div>
+  </div>
+</main>
+<script>
+function pop_up(url){
+window.open(url,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=1076,height=768,directories=no,location=no') 
+}
+</script>

4b39653cf15d368d93977f5485ef24d5d800d7b9
Christian Heinisch <chris@christian-heinisch.de>
2021-11-17 15:43:15 +0100

    Uplosd Multiple Files without Database import

--- admin/gui/content.php
+++ admin/gui/content.php
@@ -1,3 +1,20 @@
+<?php 
+if(isset($_POST['submit'])){
+ 
+ // Count total files
+ $countfiles = count($_FILES['file']['name']);
+
+ // Looping all files
+ for($i=0;$i<$countfiles;$i++){
+  $filename = $_FILES['file']['name'][$i];
+ 
+  // Upload file
+  move_uploaded_file($_FILES['file']['tmp_name'][$i],'../storage/images/original/'.$filename);
+ 
+ }
+} 
+?>
+
 </div>
     </nav>
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
@@ -5,14 +22,12 @@
     <h1 class="h2">Pictures</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
       <div class="btn-group me-2">
-        <!--<form action="#" method="post">-->
-        <a href="inc/file_uploader.php" class="btn btn-sm btn-outline-danger" onclick="pop_up(this);">Upload</a>
+        <form method='post' action='' enctype='multipart/form-data'>
+ <input type="file" name="file[]" class="btn btn-sm btn-outline-danger" id="file" multiple>
+
+ <input type='submit' name='submit' class="btn btn-sm btn-outline-danger" value='Upload'>
+</form>
       </div>
     </div>
   </div>
 </main>
-<script>
-function pop_up(url){
-window.open(url,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=1076,height=768,directories=no,location=no') 
-}
-</script>

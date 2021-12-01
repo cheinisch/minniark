1c64e34249f1f15ca18da42b701300221fd7befd
Christian Heinisch <chris@christian-heinisch.de>
2021-12-01 16:06:01 +0100

    Update update.php

---
+++ update.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+Update functions
+1. get latest zip archiv
+2. delete all data but not the storage folder
+3. unzip the latest archiv
+4. check database for an update
+5. update database
+6. reload admin interface
+*/
+
+
+// functions
+
+function get_latest_archiv()
+{
+
+}
+
+function delete_data()
+{
+
+}
+
+function unzip()
+{
+
+}
+
+function update_database()
+{
+    
+}
+
+?>

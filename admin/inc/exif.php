cc2c149408cf0f97d2df05063204054d667555ed
Christian Heinisch <chris@christian-heinisch.de>
2021-12-01 11:19:55 +0100

    Create exif.php

---
+++ admin/inc/exif.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Get All Exifdata from Picture
+*/
+function get_exif_data($picture)
+{
+
+}
+
+/*
+Public Exif functions
+*/
+
+function ip_get_exif_date()
+{
+
+}
+
+function ip_get_exif_iso()
+{
+
+}
+
+function ip_get_exif_aperture()
+{
+
+}
+
+function ip_get_exif_exposure_time()
+{
+
+}
+
+?>

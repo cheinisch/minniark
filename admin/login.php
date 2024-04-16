73e305ab9db9b7bf60b7fb708b1fe6e0a74d56c8
Christian Heinisch <chris@christian-heinisch.de>
2024-04-16 14:33:31 +0200

    mofiy login

--- admin/login.php
+++ admin/login.php
@@ -10,6 +10,7 @@ if(!@include_once('inc/functions.php')) {
 
 
   $_login = "false";
+  $_accountfile = true;
 
 	if (!isset($_GET['login']))
 	{
@@ -20,10 +21,63 @@ if(!@include_once('inc/functions.php')) {
 		$_login = "true";
 	}
 
-	if ($_login == "false")
+  if(!$_accountfile)
+  {
+    echo 'create account file';
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login Page</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.5/css/uikit.min.css" />
+      </head>
+      <body>
+
+        <div class="uk-container uk-flex uk-flex-center uk-flex-middle" style="height: 100vh;">
+          <div class="uk-card uk-card-default uk-card-body uk-width-1-3@m">
+            <h3 class="uk-card-title uk-text-center">Login</h3>
+            <form class="uk-form-stacked" action="login.php?login=login" method="get">
+              <input type="hidden" id="login" name="login" value="login" />
+              <div class="uk-margin">
+                <label class="uk-form-label" for="username">Username</label>
+                <div class="uk-form-controls">
+                  <input class="uk-input" id="username" type="text" name="username" placeholder="Your username">
+                </div>
+              </div>
+              <div class="uk-margin">
+                <label class="uk-form-label" for="username">Mailadresse</label>
+                <div class="uk-form-controls">
+                  <input class="uk-input" id="mail" type="text" name="mail" placeholder="Your Mailadress">
+                </div>
+              </div>
+              <div class="uk-margin">
+                <label class="uk-form-label" for="password">Password</label>
+                <div class="uk-form-controls">
+                  <input class="uk-input" id="password" name="password" type="password" placeholder="Your password">
+                </div>
+              </div>
+              <div class="uk-margin uk-text-center">
+                <button class="uk-button uk-button-primary" type="submit">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.5/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.5/js/uikit-icons.min.js"></script>
+      </body>
+    </html>
+
+    <?php
+  }
+
+	elseif ($_login == "false" && $_accountfile)
 	{
 
-?>
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">

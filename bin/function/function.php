8e357f58f3b8ad2e734d39782b0fe64843f2fb79
Christian Heinisch <chris@christian-heinisch.de>
2024-04-16 16:25:21 +0200

    Create and Download Loginfile

--- bin/function/function.php
+++ bin/function/function.php
@@ -7,7 +7,7 @@
 function userfile_exist()
 {
 
-    if(file_exists('conf/account.php'))
+    if(file_exists('conf/userdata.php'))
     {
         return true;
     }else{
@@ -15,6 +15,30 @@ function userfile_exist()
     }
 
     return false;
+}
+
+/*
+ * Create Account File
+ */
+
+ function downloadJSON($data, $filename) {
+    // JSON-Daten in eine Zeichenkette konvertieren
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+
+    // HTTP-Header für den Download festlegen
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/json');
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . strlen($json));
+
+    // JSON-Daten ausgeben
+    echo $json;
+
+    // Skript beenden, um unerwünschte zusätzliche Ausgaben zu verhindern
+    exit;
 }
 
 

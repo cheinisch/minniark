d325cc11ff64b97f02f103a0574919207940fe16
Christian Heinisch <chris@christian-heinisch.de>
2024-04-16 13:22:40 +0200

    Migrate Loginpage to uikit

--- admin/login.php
+++ admin/login.php
@@ -25,83 +25,44 @@ if(!@include_once('inc/functions.php')) {
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
-    <title>Signin Template · Bootstrap v5.1</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
-
-    
-
-    <!-- Bootstrap core CSS -->
-<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="assets/dist/css/signin.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.5/css/uikit.min.css" />
   </head>
-  <body class="text-center">
-    
-<main class="form-signin">
-  <form action="login.php?login=login" method="post">
-    <img class="mb-4" src="assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+  <body>
 
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" name="user" placeholder="name@example.com">
-      <label for="floatingInput">User</label>
+    <div class="uk-container uk-flex uk-flex-center uk-flex-middle" style="height: 100vh;">
+      <div class="uk-card uk-card-default uk-card-body uk-width-1-3@m">
+        <h3 class="uk-card-title uk-text-center">Login</h3>
+        <form class="uk-form-stacked">
+          <div class="uk-margin">
+            <label class="uk-form-label" for="username">Username</label>
+            <div class="uk-form-controls">
+              <input class="uk-input" id="username" type="text" placeholder="Your username">
+            </div>
+          </div>
+          <div class="uk-margin">
+            <label class="uk-form-label" for="password">Password</label>
+            <div class="uk-form-controls">
+              <input class="uk-input" id="password" type="password" placeholder="Your password">
+            </div>
+          </div>
+          <div class="uk-margin uk-text-center">
+            <button class="uk-button uk-button-primary" type="submit">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-    <?php
-      if(isset($_GET['success']))
-      {
-        if($_GET['success'] == 'false')
-        {
-    ?>
-    <p>Wrong Login Data <a href="#">Reset Passwort</a></p>
-      <?php
-        }
-      }
-      ?>
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2021-2022 Version <?php echo $ini['app_version']; ?></p>
-  </form>
-</main>
 
-
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.5/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.5/js/uikit-icons.min.js"></script>
   </body>
 </html>
+
 
 <?php
   }elseif($_GET['login'] == 'login')

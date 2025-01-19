bc8946d679c0e4b596b31a2e87518a318d2782d0
Christian Heinisch <chris@christian-heinisch.de>
2025-01-19 14:32:49 +0100

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -42,7 +42,7 @@
             <a href="#" class="block px-4 py-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Settings</a>
           </li>
           <li>
-            <a href="#" class="block px-4 py-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Logout</a>
+            <a href="login.php?logout=true" class="block px-4 py-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Logout</a>
           </li>
           <li>
             <button 

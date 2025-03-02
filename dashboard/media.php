93796c03752352cf9e5917ab7bcda0cdd5c8ef6a
Christian Heinisch <chris@christian-heinisch.de>
2025-03-02 16:13:32 +0100

    modify Sidebar

--- dashboard/media.php
+++ dashboard/media.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/sidebar.css">
     <style>
       .theme-toggle {
         width: 75px;
@@ -71,53 +73,8 @@
       ?>
       <!-- Navbar End -->
       <main class="d-flex flex-nowrap">
-        <div class="d-flex flex-column flex-shrink-0 bg-body-tertiary sidebar" style="width: 4.5rem;">
-          <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-            <li class="nav-item">
-              <a href="media.php" class="nav-link py-3 active border-bottom rounded-0" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Home"><use xlink:href="#image"/></svg>
-              </a>
-            </li>
-            <li>
-              <a href="essay.php" class="nav-link py-3 border-bottom rounded-0"  aria-current="page" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Orders"><use xlink:href="#essay"/></svg>
-              </a>
-            </li>
-            <li>
-              <a href="page.php" class="nav-link py-3 border-bottom rounded-0" title="Products" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Products"><use xlink:href="#page"/></svg>
-              </a>
-            </li>
-            <li>
-              <a href="settings.php" class="nav-link py-3 border-bottom rounded-0" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#settings"/></svg>
-              </a>
-            </li>
-          </ul>
-          <div class="dropdown border-top">
-            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-              <li>
-                <a href="login.php?logoff" class="nav-link py-3 border-bottom rounded-0" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-                  <svg class="bi pe-none logoff" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#power"/></svg>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary sidebar" style="width: 280px;">
-          <ul class="nav nav-pills flex-column mb-auto">
-            <li class="nav-item">
-              <a href="media.php" class="nav-link active link-body-emphasis" aria-current="page" >
-                View All
-              </a>
-            </li>
-            <li>
-              <a href="media-upload.php" class="nav-link">
-                Upload Image
-              </a>
-            </li>
-          </ul>
-        </div>
+        <!-- Sidebar Starts -->
+        <?php include('sidenav.php'); ?>
         <!-- Sidebars end -->
         <!-- Main Content -->
          <div class="container">
@@ -163,11 +120,6 @@
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <script src="js/sidebars.js"></script>
   </body>
 </html>

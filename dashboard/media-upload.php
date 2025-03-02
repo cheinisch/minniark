551bb51353df93914ca90f2d749c88d8d4aac3e0
Christian Heinisch <chris@christian-heinisch.de>
2025-03-02 16:21:19 +0100

    Change Sidebar

--- dashboard/media-upload.php
+++ dashboard/media-upload.php
@@ -6,6 +6,7 @@
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/sidebar.css">
   </head>
   <body>
     <!-- SVG Icons -->
@@ -47,53 +48,8 @@
       </header>
       <!-- Navbar End -->
       <main class="d-flex flex-nowrap">
-        <div class="d-flex flex-column flex-shrink-0 bg-body-tertiary sidebar" style="width: 4.5rem;">
-          <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-            <li class="nav-item">
-              <a href="media.php" class="nav-link py-3 active border-bottom rounded-0"title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Home"><use xlink:href="#image"/></svg>
-              </a>
-            </li>
-            <li>
-              <a href="essay.php" class="nav-link py-3 border-bottom rounded-0"  aria-current="page" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Orders"><use xlink:href="#essay"/></svg>
-              </a>
-            </li>
-            <li>
-              <a href="page.php" class="nav-link py-3 border-bottom rounded-0" title="Products" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Products"><use xlink:href="#page"/></svg>
-              </a>
-            </li>
-            <li>
-              <a href="settings.php" class="nav-link py-3 border-bottom rounded-0" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#settings"/></svg>
-              </a>
-            </li>
-          </ul>
-          <div class="dropdown border-top">
-            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-              <li>
-                <a href="#" class="nav-link py-3 border-bottom rounded-0" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-                  <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#power"/></svg>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary sidebar" style="width: 280px;">
-          <ul class="nav nav-pills flex-column mb-auto">
-            <li class="nav-item">
-              <a href="media.php" class="nav-link " >
-                View All
-              </a>
-            </li>
-            <li>
-              <a href="media-upload.php" class="nav-link active link-body-emphasis"  aria-current="page">
-                Upload Image
-              </a>
-            </li>
-          </ul>
-        </div>
+        <!-- Sidebar Starts -->
+        <?php include('sidenav.php'); ?>
         <!-- Sidebars end -->
         <!-- Main Content -->
          <div class="content">
@@ -104,6 +60,6 @@
     <!-- Wrapper End -->
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <script src="js/sidebars.js"></script>
   </body>
 </html>
--- dashboard/sidenav.php
+++ dashboard/sidenav.php
@@ -2,46 +2,41 @@
     <ul class="list-unstyled ps-0">
       <li class="mb-1">
         <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-          Home
+          Media
         </button>
         <div class="collapse show" id="home-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Updates</a></li>
-            <li><a href="#" class="link-dark rounded">Reports</a></li>
+            <li><a href="media.php" class="link-dark rounded">View all</a></li>
+            <li><a href="media-upload.php" class="link-dark rounded">Upload Image</a></li>
           </ul>
         </div>
       </li>
       <li class="mb-1">
         <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-          Dashboard
+          Essays
         </button>
         <div class="collapse" id="dashboard-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Weekly</a></li>
-            <li><a href="#" class="link-dark rounded">Monthly</a></li>
-            <li><a href="#" class="link-dark rounded">Annually</a></li>
+            <li><a href="essay.php" class="link-dark rounded">View all</a></li>
+            <li><a href="edit-essay.php" class="link-dark rounded">New Essay</a></li>
           </ul>
         </div>
       </li>
       <li class="mb-1">
         <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-          Orders
+          Pages
         </button>
         <div class="collapse" id="orders-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">New</a></li>
-            <li><a href="#" class="link-dark rounded">Processed</a></li>
-            <li><a href="#" class="link-dark rounded">Shipped</a></li>
-            <li><a href="#" class="link-dark rounded">Returned</a></li>
+            <li><a href="#" class="link-dark rounded">View All</a></li>
+            <li><a href="#" class="link-dark rounded">New Page</a></li>
           </ul>
         </div>
       </li>
       <li class="border-top my-3"></li>
       <li class="mb-1">
         <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-          Account
+          Settings
         </button>
         <div class="collapse" id="account-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">

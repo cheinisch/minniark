389309c78ab80f032f7eda5b651ea019950b8117
Christian Heinisch <chris@christian-heinisch.de>
2025-03-03 20:32:16 +0100

    Add dummy content to essay

--- dashboard/essay.php
+++ dashboard/essay.php
@@ -86,7 +86,33 @@
     <main class="py-10">
       <div class="px-4 sm:px-6 lg:px-8">
         <!-- Your content -->
-         Essays
+        <div class="bg-white py-1 sm:py-1">
+          <div class="mx-auto px-6 lg:px-8">
+              <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Essays</h2>
+            <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+              <?php for ($i = 1; $i <= 10; $i++) { ?>
+              <article class="flex max-w-xl flex-col items-start justify-between">
+                <div class="flex items-center gap-x-4 text-xs">
+                  <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+                  <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
+                </div>
+                <div class="group relative">
+                  <h3 class="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+                    <a href="#">
+                      <span class="absolute inset-0"></span>
+                      Boost your conversion rate
+                    </a>
+                  </h3>
+                  <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                </div>
+              </article>
+              <?php } ?>
+              <!-- More posts... -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Your content end-->
       </div>
     </main>
   </div>

6c5f3b129f2df4e7e9e5f5e673b5ea2179bc6664
Christian Heinisch <chris@christian-heinisch.de>
2025-03-11 13:35:05 +0100

    Add Title to Image Detail Page

--- dashboard/media-detail.php
+++ dashboard/media-detail.php
@@ -54,6 +54,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $title;?> // ImagePortfolio</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 
     <!-- Leaflet.js für OSM -->

2779bf30f90eaedefb7b08a378c9ce2996991f73
Christian Heinisch <chris@christian-heinisch.de>
2025-04-16 16:38:08 +0200

    Update login.php

--- dashboard_v2/login/login.php
+++ dashboard_v2/login/login.php
@@ -71,6 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="de">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login Fehler</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.10/dist/css/uikit.min.css" />
 </head>

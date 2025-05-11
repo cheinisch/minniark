9d24495731cc9a55c1960d62da0f9cbff6918376
Christian Heinisch <chris@christian-heinisch.de>
2025-05-11 16:41:47 +0200

    Update pages.php

--- dashboard/pages.php
+++ dashboard/pages.php
@@ -55,12 +55,12 @@
                     </div>
                     <div class="flex items-center">
                       <div class="shrink-0">
-                        <a href="blog-detail.php?post=new">
+                        <a href="page-detail.php?post=new">
                           <button type="button" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-sky-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
                             <svg class="-ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                               <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
                             </svg>
-                            Create new Post
+                            Create new Page
                           </button>
                         </a>
                       </div>

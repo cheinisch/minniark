31985c2b90d707f5cceb67e9e002a6b1cd4d7a20
Christian Heinisch <chris@christian-heinisch.de>
2025-05-11 14:42:41 +0200

    add pages

--- dashboard/pages.php
+++ dashboard/pages.php
@@ -1,6 +1,8 @@
 <?php
-
   require_once( __DIR__ . "/../functions/function_backend.php");
+  security_checklogin();
+  // Prüfen, ob ein bestimmtes Jahr übergeben wurde
+  $filterYear = isset($_GET['year']) ? $_GET['year'] : null;
 
 ?>
 
@@ -9,20 +11,20 @@
     <head>      
         <meta charset="UTF-8">        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Images - <?php echo get_sitename(); ?></title>
+        <title>Pages - <?php echo get_sitename(); ?></title>
 
         <!-- Tailwind CSS -->
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
     <body class="min-h-screen flex flex-col">
         <header>
-            <nav class="bg-stone-900 shadow-sm">
+        <nav class="bg-neutral-200 dark:bg-gray-950 shadow-sm">
                 <div class="mx-auto max-w-12xl px-4 sm:px-6 lg:px-8">
                   <div class="flex h-16 justify-between">
                     <div class="flex">
                       <div class="mr-2 -ml-2 flex items-center md:hidden">
                         <!-- Mobile menu button -->
-                        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:ring-2 focus:ring-sky-500 focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
+                        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-600 dark:text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:ring-2 focus:ring-sky-500 focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
                           <span class="absolute -inset-0.5"></span>
                           <span class="sr-only">Open main menu</span>
                           <!--
@@ -45,37 +47,22 @@
                       </div>
                       <div class="hidden md:ml-6 md:flex md:space-x-8">
                         <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-                        <a href="dashboard.php" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-300 hover:border-sky-400 hover:text-sky-400">Dashboard</a>
-                        <a href="media.php" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-300 hover:border-sky-400 hover:text-sky-400">Images</a>
-                        <a href="blog.php" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-300 hover:border-sky-400 hover:text-sky-400">Blogposts</a>
-                        <a href="pages.php" class="inline-flex items-center border-b-2 border-sky-400 px-1 pt-1 text-sm font-medium text-sky-400">Pages</a>
+                        <a href="dashboard.php" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-base font-medium text-gray-600 dark:text-gray-300 hover:border-sky-500 hover:text-sky-500">Dashboard</a>
+                        <a href="media.php" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-base font-medium text-gray-600 dark:text-gray-300 hover:border-sky-500 hover:text-sky-500">Images</a>
+                        <a href="blog.php" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-base font-medium text-gray-600 dark:text-gray-300 hover:border-sky-500 hover:text-sky-500">Blogposts</a>
+                        <a href="pages.php" class="inline-flex items-center border-b-2 border-sky-500 px-1 pt-1 text-base font-medium text-sky-500">Pages</a>
                       </div>
                     </div>
                     <div class="flex items-center">
-                      <div class="hidden md:block rounded-lg px-10 p-2 shadow-sm max-w-[300px]">
-                         <!-- Label + Wert nebeneinander -->
-                        <div class="flex justify-between items-center text-xs text-gray-300 mb-1">
-                          <span>Bildbreite:</span>
-                          <span id="range-value">300px</span>
-                        </div>
-
-                        <!-- Range-Slider -->
-                        <input
-                          class="w-full accent-sky-400"
-                          type="range"
-                          value="300"
-                          min="20"
-                          max="500"
-                          oninput="document.getElementById('range-value').innerText = this.value + 'px'"
-                        >
-                      </div>
                       <div class="shrink-0">
-                        <button type="button" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-400 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-sky-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
-                          <svg class="-ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                            <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-                          </svg>
-                          Upload new Image
-                        </button>
+                        <a href="blog-detail.php?post=new">
+                          <button type="button" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-sky-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+                            <svg class="-ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                              <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+                            </svg>
+                            Create new Post
+                          </button>
+                        </a>
                       </div>
                       <div class="hidden md:ml-4 md:flex md:shrink-0 md:items-center">
                         <button type="button" class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:outline-hidden">
@@ -124,8 +111,8 @@
                     <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
                     <a href="dashboard.php" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pr-6 sm:pl-5">Dashboard</a>
                     <a href="media.php" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pr-6 sm:pl-5">Images</a>
-                    <a href="blog.php" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pr-6 sm:pl-5">Blogposts</a>
-                    <a href="pages.php" class="block border-l-4 border-sky-400 py-2 pr-4 pl-3 text-base font-medium text-sky-400 sm:pr-6 sm:pl-5">Pages</a>
+                    <a href="blog.php" class="block border-l-4 border-sky-500 py-2 pr-4 pl-3 text-base font-medium text-sky-500 sm:pr-6 sm:pl-5">Blogposts</a>
+                    <a href="pages.php" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pr-6 sm:pl-5">Pages</a>
                   </div>
                   <div class="border-t border-gray-500 pt-4 pb-3">
                     <div class="mt-3 space-y-1">
@@ -167,23 +154,56 @@
               
         </header>
         <div class="flex flex-1">
-          <aside class="hidden md:block max-w-xs w-full bg-stone-900 overflow-auto flex-1">
-              <nav class="flex flex-1 flex-col px-15 pt-5 text-gray-300 text-sm font-medium" aria-label="Sidebar">
-                  <ul role="list" class="-mx-2 space-y-1">
-                    <li>Content</li>
-                    <ul class="px-5">
-                      <li><a href="#" class="text-gray-400 hover:text-sky-400">All Photos</a></li>
-                    </ul>
-                    <li>
-                      Albums
-                    </li>
-                    <ul class="px-5">
-                      <li><a href="#" class="text-gray-400 hover:text-sky-400">Album 1</a></li>
-                    </ul>                    
+          <aside class="hidden md:block max-w-[280px] w-full bg-neutral-200 dark:bg-gray-950 overflow-auto flex-1">
+            <nav class="flex flex-1 flex-col pt-5 px-15 text-gray-600 text-base font-medium" aria-label="Sidebar">
+                <ul role="list" class="-mx-2 space-y-1">
+                  <li>Content</li>
+                  <ul class="px-5">
+                    <li><a href="?" class="text-gray-400 hover:text-sky-500">All Posts (<?php echo count_pages(); ?>)</a></li>
                   </ul>
-                </nav>
+                  <li>Year</li>
+                  <ul class="px-5">
+                    <?php get_pageyearlist(false); ?>
+                  </ul>                  
+                </ul>
+              </nav>
           </aside>
-          <main class="flex-1 bg-stone-800 p-6 overflow-auto">
+          <main class="flex-1 bg-white dark:bg-neutral-900 p-6 overflow-auto">
+          
+  <div class="max-w-full px-6 lg:px-8">
+    <div class="max-w-8xl">
+      <div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+        <?php
+          $pages = get_pages($filterYear);
+          foreach($pages as $page)
+          {
+              ?>
+        <article class="relative isolate flex flex-col gap-8 lg:flex-row">
+          <div class="relative aspect-video sm:aspect-2/1 lg:aspect-square lg:w-64 lg:shrink-0">
+            <img src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80" alt="" class="absolute inset-0 size-full rounded-2xl bg-gray-50 object-cover">
+            <div class="absolute inset-0 rounded-2xl ring-1 ring-gray-900/10 ring-inset"></div>
+          </div>
+          <div>
+            <div class="flex items-center gap-x-4 text-xs">
+              <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+            </div>
+            <div class="group relative max-w-5xl">
+              <h3 class="mt-3 text-lg/6 font-semibold text-gray-900 dark:text-gray-400 group-hover:text-gray-600 dark:group-hover:text-gray-200">
+                <a href="page-detail.php?edit=<?php echo $page['source_path']; ?>">
+                  <span class="absolute inset-0"></span>
+                  <?php echo $page['title']; ?>
+                </a>
+              </h3>
+              <p class="mt-5 text-sm/6 text-gray-600"><?php echo $page['content']; ?></p>
+            </div>
+          </div>
+        </article>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+
+
 
           </main>
         </div>

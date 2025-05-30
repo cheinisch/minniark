624d8d84615554b4e118f0b0749ab0b220400416
Christian Heinisch <chris@christian-heinisch.de>
2025-05-30 22:47:27 +0200

    Update user.php

--- functions/backend/user.php
+++ functions/backend/user.php
@@ -1,7 +1,11 @@
 <?php
 
-function saveNewUser($username, $mail, $password):bool
-{
-$slug = generateSlug($username);
-return false;
-}
+    function saveNewUser($username, $mail, $password):bool
+    {
+    
+        $slug = generateSlug($username);
+
+        $ymlDir = __DIR__.'/../../userdata/config/user/';
+        $ymlFile = __DIR__.'/../../userdata/config/user/'.$slug.'.yml';
+    return false;
+    }

7efd191c07399342587fc1056bfcd5541e0638d5
Christian Heinisch <chris@christian-heinisch.de>
2025-07-10 16:32:54 +0200

    Update dashboard-theme.php

--- dashboard/dashboard-theme.php
+++ dashboard/dashboard-theme.php
@@ -97,12 +97,12 @@
                             </svg>
                             Theme Updates available
                         </button>
-                        <button type="button" id="update-btn-docker" class="relative inline-flex items-center gap-x-1.5 bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+                        <a href="?search" class="relative inline-flex items-center gap-x-1.5 bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
                             <svg class="-ml-0.5 w-5 h-5" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
                               <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
                             </svg>
                             Search new Themes
-                        </button>
+                        </a>
                     </div>
                       <div class="hidden md:ml-4 md:flex md:shrink-0 md:items-center">
                         <button type="button" class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:outline-hidden">

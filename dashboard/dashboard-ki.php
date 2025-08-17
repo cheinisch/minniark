cec2080b1eddc25f754cf80f3367a99c95454866
Christian Heinisch <chris@christian-heinisch.de>
2025-08-17 15:20:50 +0200

    Update dashboard-ki.php

--- dashboard/dashboard-ki.php
+++ dashboard/dashboard-ki.php
@@ -169,11 +169,6 @@
 
                 <form class="md:col-span-2" id="change-sitedata-form">
                   <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-
-                    <div class="sm:col-span-full">
-                      <span for="site-name" class="block text-sm/6 font-medium text-gray-700 dark:text-white">AI API KEY: <?php echo getOpenAIKey(get_license()); ?></span>
-                      
-                    </div>
                     <div class="sm:col-span-full">
                       <label for="site-name" class="block text-sm/6 font-medium text-gray-700 dark:text-white">Select AI </label>
                       <select class="block w-full  bg-white/5 px-3 py-1.5 text-base text-gray-700 dark:text-white outline-1 -outline-offset-1 outline-gray-500 dark:outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-sky-500 sm:text-sm/6">
